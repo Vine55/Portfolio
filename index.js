0e6dd199af694457ef2a715a25d2d7d7c5a61170
Vine55 <Nnachetaebuka@gmail.com>
2026-01-08 02:04:36 +0100

    m

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ moreButtons.forEach(button => {
   button.addEventListener("click", () => {
     const description = button.nextElementSibling;
     description.classList.toggle("hidden");
+    if(button.textContent=='More') {
+      button.textContent='Hide'
+    }
+    else {
+      button.textContent='More'
+
+    }
   });
 });
